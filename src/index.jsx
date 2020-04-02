e545fe9560037c8af33557efe39e26f3ffdd46a4
Qler-gra <karasinski.kacper@outlook.com>
2020-04-02 16:54:01 +0200

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import { store } from './reducers/index.js';
+
+import Home from './components/Home/Home.jsx';
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />} />
+      </Switch>
+    </Router>
+  </Provider>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
