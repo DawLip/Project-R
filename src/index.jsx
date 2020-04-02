f85a546c6b8197a3a392bf5763713055501dcbb6
Qler-gra <karasinski.kacper@outlook.com>
2020-04-02 18:13:55 +0200

    update index

--- src/index.jsx
+++ src/index.jsx
@@ -7,12 +7,16 @@ import { Provider } from 'react-redux';
 import { store } from './reducers/index.js';
 
 import Home from './components/Home/Home.jsx';
+import LoginPage from './components/LoginPage/LoginPage.jsx';
+
+import './style.sass';
 
 const App = () => (
   <Provider store={store}>
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />} />
+        <Route path="/login" component={LoginPage} />} />
       </Switch>
     </Router>
   </Provider>
